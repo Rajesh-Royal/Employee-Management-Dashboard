27abbbcf7fbf61c67e40a3a2499c3e3de9a1d336
unknown <rajeshroyal896@gmail.com>
2021-06-11 12:55:26 +0530

    salary folder structure

--- server/src/app/employee/employee-repository.service.ts
+++ server/src/app/employee/employee-repository.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { EmployeeCreateMutationModel } from './employee.mutation.model';
+import { EmployeeCreateMutationModel } from './services/employee.mutation.model';
 import { EmployeeType } from '../shared/employee.type';
 
 @Injectable()
