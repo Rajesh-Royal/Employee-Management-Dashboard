82c35a6707665fbaed4dfdb0b21345a301a6019e
unknown <rajeshroyal896@gmail.com>
2021-06-11 08:28:42 +0530

    graphql initialized

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
+import { AppResolvers } from './app.resolvers';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      playground: true,
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    AppResolvers
+  ],
 })
 export class AppModule {}
