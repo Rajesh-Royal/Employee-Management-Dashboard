2f39861797d05c2e114010c4fd023b1cc2402432
unknown <rajeshroyal896@gmail.com>
2021-06-13 02:48:56 +0530

    employee queries added

---
+++ client/src/core/gql-operations/query/employee-list-read-query.js
@@ -0,0 +1,22 @@
+import { gql } from "@apollo/client";
+
+export const EMPLOYEE_LIST_READ = gql`
+  query employeeListRead {
+    employeeListRead {
+      _id
+      firstName
+      lastName
+      email
+      city
+      ctc
+      salary {
+        _id
+        da
+        pa
+        hra
+        pt
+        epf
+      }
+    }
+  }
+`;
