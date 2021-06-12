637e0a990fba7b156783cadc67d2d4adbc7875a0
unknown <rajeshroyal896@gmail.com>
2021-06-12 21:55:34 +0530

    title updated

--- client/src/Pages/404.js
+++ client/src/Pages/404.js
@@ -9,7 +9,7 @@ const Page404 = () => {
   return (
     <div className="flex flex-col items-center">
       <Helmet>
-        <title>404</title>
+        <title>404 - Page not found</title>
       </Helmet>
       <XOctagon className="mt-8 w-12 h-12 text-purple-500" aria-hidden="true" />
       <h1 className="dark:text-gray-200 text-gray-700 text-6xl font-semibold">404</h1>
