a97462cb4c858e73b029007d785f02d1d74739cf
unknown <rajeshroyal896@gmail.com>
2021-06-12 21:41:57 +0530

    Main layout of app, root component

---
+++ client/src/Container/Layout.js
@@ -0,0 +1,40 @@
+import React, { lazy, Suspense } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+
+import GlobalErrorBoundary from "../Components/GlobalErrorBoundary";
+import ThemedSuspense from "../Components/ThemedSuspense";
+import Container from "./Container";
+import Routes from "../Routes/Routes";
+import Header from "../Components/Header";
+
+const Page404 = lazy(() => import("../Pages/404"));
+
+function Layout() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Container>
+        <GlobalErrorBoundary>
+          <Suspense fallback={<ThemedSuspense />}>
+            <Switch>
+              {Routes.map((route, i) => {
+                return route.component ? (
+                  <Route
+                    key={i}
+                    exact
+                    path={route.path}
+                    render={(props) => <route.component {...props} />}
+                  />
+                ) : null;
+              })}
+              <Redirect exact from="/" to="/dashboard" />
+              <Route component={Page404} />
+            </Switch>
+          </Suspense>
+        </GlobalErrorBoundary>
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default Layout;
