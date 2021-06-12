4ee37efee226045cdfcb765151bee0095fc13b19
unknown <rajeshroyal896@gmail.com>
2021-06-13 02:46:21 +0530

    apollo configuration

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
 import "./assets/global-css/tailwind.css";
 import App from "./App";
+import { projectData } from "./Data/data";
+
+const client = new ApolloClient({
+  uri: projectData.graphqlServer,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
