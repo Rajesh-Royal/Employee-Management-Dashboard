0663357a054a3f31cc5d03a6c7a7779e308612ac
unknown <rajeshroyal896@gmail.com>
2021-06-12 18:27:09 +0530

    code linted

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
     <div className="max-w-full bg-purple-500">
-        <header className="max-w-7xl m-auto py-3 px-3 xl:px-0 flex items-center justify-between text-white">
-          <h1 className="font-medium">Employee Dashboard</h1>
-          <p>Github</p>
-        </header>
+      <header className="max-w-7xl m-auto py-3 px-3 xl:px-0 flex items-center justify-between text-white">
+        <h1 className="font-medium">Employee Dashboard</h1>
+        <p>Github</p>
+      </header>
     </div>
   );
 }
