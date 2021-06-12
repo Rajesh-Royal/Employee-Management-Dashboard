900c65223f859387acf9b4c68a00e25ac6d4caac
unknown <rajeshroyal896@gmail.com>
2021-06-12 21:54:12 +0530

    unsafeComponentWillMount error fixed with react-helmet-async

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
 import Layout from "./Container/Layout";
 
 function App(props) {
   return (
     <React.Fragment>
-      <Router>
-        <Switch>
-          <Layout />
-        </Switch>
-      </Router>
+      <HelmetProvider>
+        <Router>
+          <Switch>
+            <Layout />
+          </Switch>
+        </Router>
+      </HelmetProvider>
     </React.Fragment>
   );
 }
