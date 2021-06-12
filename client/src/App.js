ae36256012bb7347f75f34a755da756d124587ee
unknown <rajeshroyal896@gmail.com>
2021-06-12 21:38:06 +0530

    react-router, feather-icons, helmet installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 
-function App() {
+import Layout from "./Container/Layout";
+
+function App(props) {
   return (
-    <div className="max-w-full bg-purple-500">
-      <header className="max-w-7xl m-auto py-3 px-3 xl:px-0 flex items-center justify-between text-white">
-        <h1 className="font-medium">Employee Dashboard</h1>
-        <p>Github</p>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Switch>
+          <Layout />
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
