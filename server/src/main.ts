396f60b3ce8f4614f51267e7f07c0c92bbc5515a
unknown <rajeshroyal896@gmail.com>
2021-06-13 21:28:41 +0530

    sdfdf

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT || 4000 || 5000);
-  // adding this to run service again
-// available
 }
 bootstrap();
