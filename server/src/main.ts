aadefbfe0d910e10a6c37f4cb4320828b6ab3d1c
unknown <rajeshroyal896@gmail.com>
2021-06-13 21:30:42 +0530

    sdfsdf

---
+++ server/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.listen(process.env.PORT || 4000 || 5000);
+}
+bootstrap();
