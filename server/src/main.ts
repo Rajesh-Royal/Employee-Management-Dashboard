1c5139c93a6440f304e69adb73b18fad37a4965a
unknown <rajeshroyal896@gmail.com>
2021-06-13 20:50:06 +0530

    heroku not working

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT || 4000)
+  await app.listen(process.env.PORT || 4000);
+  // adding this to run service again
+
 }
 bootstrap();
