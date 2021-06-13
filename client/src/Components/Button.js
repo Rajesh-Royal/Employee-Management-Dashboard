35c4eddede320af02ba1be3f6c92af8bc2216901
unknown <rajeshroyal896@gmail.com>
2021-06-13 18:42:04 +0530

    modal style and utility functions

--- client/src/Components/Button.js
+++ client/src/Components/Button.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-const Button = ({ children, className = "", ...buttonProps }) => {
+const Button = ({ children, className = "", disabled, ...buttonProps }) => {
   return (
     <button
-      className={`py-2.5 px-5 bg-purple-500 rounded-md text-white font-medium focus:outline-none hover:bg-purple-600 transition-all duration-75 ${className}`}
-      {...buttonProps}>
+      className={`py-2.5 px-5 rounded-md text-white font-medium focus:outline-none transition-all duration-75 ${className} ${
+        disabled ? "bg-gray-200 cursor-not-allowed" : "bg-purple-500 hover:bg-purple-600 "
+      }`}
+      {...buttonProps}
+      disabled={disabled}>
       {children}
     </button>
   );
---
+++ client/src/utility/ModalStyle.js
@@ -0,0 +1,12 @@
+export const modalStyle = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    border: "0px",
+    background: "transparent",
+  },
+};
