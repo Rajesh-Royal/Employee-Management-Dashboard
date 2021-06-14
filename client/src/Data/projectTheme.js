57ca60641920615b932d23004d614ae84b3abf33
unknown <rajeshroyal896@gmail.com>
2021-06-14 10:18:48 +0530

    project theme random color not work in build

--- client/src/Data/projectTheme.js
+++ client/src/Data/projectTheme.js
@@ -1,8 +1,17 @@
-const colorsArray = ["purple", "blue", "green"];
-const color = colorsArray[Math.floor(Math.random() * colorsArray.length)];
+// random theme only work in developement
+
+// const colorsArray = ["indigo", "purple", "blue", "green"];
+// const color = colorsArray[Math.floor(Math.random() * colorsArray.length)];
+// export const projectTheme = {
+//   background: `bg-${color}-500` || "bg-purple-500",
+//   borderColor: `border-${color}-500` || "border-purple-500",
+//   textColor: `text-${color}-500` || "text-purple-500",
+//   closeXButtonColor: `text-${color}-500` || "text-purple-400",
+// };
+
 export const projectTheme = {
-  background: `bg-${color}-500` || "bg-purple-500",
-  borderColor: `border-${color}-500` || "border-purple-500",
-  textColor: `text-${color}-500` || "text-purple-500",
-  closeXButtonColor: `text-${color}-500` || "text-purple-400",
+  background: "bg-blue-500",
+  borderColor: "border-blue-500",
+  textColor: "text-blue-500",
+  closeXButtonColor: "text-blue-500",
 };
