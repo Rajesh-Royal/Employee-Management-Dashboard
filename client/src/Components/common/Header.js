41a7cd3d8be88b8eff562cea101e107c01985029
unknown <rajeshroyal896@gmail.com>
2021-06-21 09:32:33 +0530

    user login frontend

--- client/src/Components/common/Header.js
+++ client/src/Components/common/Header.js
@@ -1,15 +1,18 @@
 import React from "react";
-import Container from "../../Container/Container";
 import { GitHub } from "react-feather";
+import { Link } from "react-router-dom";
+import Container from "../../Container/Container";
 import { projectData } from "../../Data/data";
 import { projectTheme } from "../../Data/projectTheme";
 import DarkModeToggle from "./DarkModeToggle";
 
 const Header = () => {
   return (
-    <Container className={`py-3 max-w-full ${projectTheme.background}`}>
+    <Container className={`py-3 max-w-full ${projectTheme.background} header`}>
       <header className="max-w-7xl mx-auto flex items-center justify-between text-gray-50">
-        <h1 className="font-extrabold text-xl">Employee Dashboard</h1>
+        <Link to="/">
+          <h1 className="font-extrabold text-xl">Employee Dashboard</h1>
+        </Link>
         <div className="flex flex-end space-x-6">
           <a href={`${projectData.repositoryUrl}`} target="_blank" rel="noreferrer">
             <GitHub />
--- client/src/Pages/404.js
+++ client/src/Pages/404.js
@@ -1,30 +1,33 @@
 import React from "react";
 import { XOctagon } from "react-feather";
-import { useHistory } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
+import { useHistory } from "react-router-dom";
+import Container from "../Container/Container";
 import { projectTheme } from "../Data/projectTheme";
 
 const Page404 = () => {
   const history = useHistory();
 
   return (
-    <div className="flex flex-col items-center">
-      <Helmet>
-        <title>404 - Page not found</title>
-      </Helmet>
-      <XOctagon className={`mt-8 w-12 h-12 ${projectTheme.textColor}`} aria-hidden="true" />
-      <h1 className="dark:text-gray-200 text-gray-700 text-6xl font-semibold">404</h1>
-      <p className="dark:text-gray-300 text-gray-700">
-        Page not found.{" "}
-        <button
-          type="button"
-          className={`dark:text-green-400 ${projectTheme.textColor} hover:underline`}
-          onClick={() => history.goBack()}>
-          Go back
-        </button>
-        .
-      </p>
-    </div>
+    <Container>
+      <div className="flex flex-col items-center space-y-6 w-full justify-items-center">
+        <Helmet>
+          <title>404 - Page not found</title>
+        </Helmet>
+        <XOctagon className={`mt-8 w-12 h-12 ${projectTheme.textColor}`} aria-hidden="true" />
+        <h1 className="dark:text-gray-200 text-gray-700 text-6xl font-semibold">404</h1>
+        <p className="dark:text-gray-300 text-gray-700">
+          Page not found.{" "}
+          <button
+            type="button"
+            className={`dark:text-green-400 ${projectTheme.textColor} hover:underline`}
+            onClick={() => history.goBack()}>
+            Go back
+          </button>
+          .
+        </p>
+      </div>
+    </Container>
   );
 };
 
