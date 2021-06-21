1b8b8ecd400737d306876b4099ed4b495462ad03
unknown <rajeshroyal896@gmail.com>
2021-06-21 10:27:18 +0530

    use auth guard

--- server/src/app/employee/employee.resolvers.ts
+++ server/src/app/employee/employee.resolvers.ts
@@ -1,11 +1,13 @@
+import { UseGuards } from "@nestjs/common";
 import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
-import { EmployeeCreateMutationModel } from "./services/employee-create.mutation.model"
-import { EmployeeCreateMutationService } from "./services/employee-create.mutation.service";
+import { GqlAuthGuard } from "../auth/guards/auth.guard";
 import { EmployeeType } from "../shared/employee.type";
-import { EmployeeListReadQueryService } from "./services/employee-list-read.query.service";
+import { EmployeeCreateMutationModel } from "./services/employee-create.mutation.model";
+import { EmployeeCreateMutationService } from "./services/employee-create.mutation.service";
 import { EmployeeReadQueryModel } from "./services/employee-list-read-query.model";
-import { EmployeeReadQueryService } from "./services/employee-read-query.service";
+import { EmployeeListReadQueryService } from "./services/employee-list-read.query.service";
 import { SingleEmployeeReadQueryModel } from "./services/employee-read-query.model";
+import { EmployeeReadQueryService } from "./services/employee-read-query.service";
 
 @Resolver()
 export class EmployeeResolvers {
@@ -17,17 +19,20 @@ export class EmployeeResolvers {
     ) {
   }
 
+  @UseGuards(GqlAuthGuard)
   @Query(() => [EmployeeReadQueryModel])
   public employeeListRead(): GraphQLResolverResult<EmployeeType[]> {
     return this.employeeListReadQueryService.serve().then(result => result);
   }
 
+  @UseGuards(GqlAuthGuard)
   @Query(() => EmployeeReadQueryModel)
   public employeeRead(@Args() arguments_: SingleEmployeeReadQueryModel): GraphQLResolverResult<EmployeeType> {
     const operation = new SingleEmployeeReadQueryModel(arguments_)
     return this.employeeReadQueryService.serve(operation).then(result => result);
   }
 
+  @UseGuards(GqlAuthGuard)
   @Mutation(() => EmployeeCreateMutationModel)
   public employeeCreate(@Args() arguments_: EmployeeCreateMutationModel): GraphQLResolverResult<EmployeeType> {
     const operation = new EmployeeCreateMutationModel(arguments_);
