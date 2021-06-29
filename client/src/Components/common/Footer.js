5b89d1c5b1709bd34b62512eac4010df4d9c264a
unknown <rajeshroyal896@gmail.com>
2021-06-29 12:04:09 +0530

    position fixed right

--- client/src/Components/common/Footer.js
+++ client/src/Components/common/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="max-w-sm absolute bottom-0 right-0 bg-gray-800 mx-auto flex justify-items-center space-y-1 p-2 flex-col text-gray-50 text-xs">
+    <footer className="max-w-sm fixed bottom-0 right-0 bg-gray-800 mx-auto flex justify-items-center space-y-1 p-2 flex-col text-gray-50 text-xs">
       <a href="https://rajeshroyal.com" target="_blank" rel="noreferrer">
         <p>@Rajesh.royal</p>
       </a>
